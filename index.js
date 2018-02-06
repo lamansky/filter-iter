93ff0486d2c57ab6109fb00040766eadd48e46f8
John Lamansky <lamansky@users.noreply.github.com>
2018-02-06 23:29:04 +0100

    Update dependencies

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict'
 
 const isPlainObject = require('is-plain-object')
-const PossibleFunction = require('possible-function')
+const pfn = require('pfn')
 
 module.exports = function * filterIterable (iter, test, options) {
   if (isPlainObject(test) && !isPlainObject(options)) options = test
   const {limit = Infinity} = options || {}
-  test = PossibleFunction(test)
+  test = pfn(test)
   let i = 0
   for (const item of iter) {
     if (test(item)) {
